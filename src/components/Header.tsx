742fc25cde6f43730badb9eff844618a3d3ebbd7
Gabriel Borges <gbdesigns13@gmail.com>
2021-04-23 10:21:18 -0300

    Aula 03 finalizada 🚀💜

---
+++ src/components/Header.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import colors from '../../styles/colors';
+import fonts from '../../styles/fonts';
+
+import userImg from '../assets/gabriel.png';
+
+export function Header() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.greeting}>Olá,</Text>
+        <Text style={styles.userName}>Gabriel</Text>
+      </View>
+
+      <Image source={userImg} style={styles.image}/>
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: getStatusBarHeight(),
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 35
+  },
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text,
+  },
+  userName: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight:40
+  }
+});
--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/core';
 import React from 'react';
 import {
   SafeAreaView,
@@ -11,6 +12,12 @@ import { Button } from '../components/Button';
 
 
 export function Confirmation() {
+  const navigation = useNavigation();
+
+  function handleMoveOn() {
+    navigation.navigate('PlantSelect')
+  }
+
   return(
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
@@ -25,7 +32,7 @@ export function Confirmation() {
         </Text>
 
         <View style={styles.footer}>
-          <Button title="Começar" />
+          <Button title="Começar" onPress={handleMoveOn}/>
         </View>
 
       </View>
