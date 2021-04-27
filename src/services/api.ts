c5b6c770d44c0814cdfe253ebd7c75675ef9d5bb
Gabriel Borges <gbdesigns13@gmail.com>
2021-04-27 11:00:34 -0300

    Add fake api URL 🚀💜

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://172.19.66.60:3333/',
+  baseURL: 'https://my-json-server.typicode.com/gbdev13/plantmanager/',
 })
 
 export default api;
